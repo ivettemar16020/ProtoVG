68839b13c96a5eaaa9f878b4b6c16790bf829dae
samanthaduarte <samanthaduarte>
2018-04-25 14:51:34 -0600

    sonido arreglado

--- Prototipo/Assets/Animationtramposo.cs
+++ Prototipo/Assets/Animationtramposo.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Animationtramposo : MonoBehaviour {
 	private Animator anim;
+	public AudioClip sonido;
+	//public AudioSource as;
+
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
 		anim.SetBool("activate",false);
+		GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = sonido;
 	}
 
 	void OnTriggerEnter(Collider other){
+		GetComponent<AudioSource>().Play();
 		if (other.tag == "Player"){
 			anim.SetBool("activate",true);
 			Score.instance.VidasCount();
+			//as.PlayOneShot(sonido, 0.7F);
 		}
 		if (other.tag != "Player"){
 			anim.SetBool("activate", false);
