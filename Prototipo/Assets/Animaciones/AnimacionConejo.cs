d8003ae00efa182b71f3459e2f0f3dda3a5b9b51
samanthaduarte <samanthaduarte>
2018-05-02 17:06:44 -0600

    conejo caminante semi terminado

---
+++ Prototipo/Assets/Animaciones/AnimacionConejo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionConejo : MonoBehaviour {
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.CrossFade("quieto", 0.1f);
+		
+		while (Input.GetKeyDown(KeyCode.B)){
+			anim.CrossFade("caminar", 0.1f);
+		}
+	}
+}
