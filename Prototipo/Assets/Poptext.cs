4eb7dc3670792296358404ffceeca0f742c5b290
Ivette <car16020@uvg.edu.gt>
2018-05-14 01:59:59 -0600

    Changes

---
+++ Prototipo/Assets/Poptext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Poptext : MonoBehaviour {
+	public Text myText; 
+
+	//OnStart
+	void Start () {
+		Invoke("DisableText", 5f);//invoke after 5 seconds
+	}
+	
+	void DisableText()
+	{
+		myText.enabled = false; 
+	}
+}
